6f5663c595f44b9ca5cb5b57203741fc84a2808c
Fabio <sstrmn1@gmail.com>
2023-04-19 13:55:57 -0400

    Apagar el autoComplete en los textfields

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, TextField, Grid } from '@mui/material';
 import './index.css';
+import Persona from "./clases/persona";
 
 
 function App() {
@@ -17,43 +18,59 @@ function App() {
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField label="Primer Nombre" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Segundo Nombre" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Primer Apellido" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Segundo Apellido" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Fecha de Nacimiento" type="date" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Estado Civil" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Padres" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Hijos" variant="outlined" fullWidth InputLabelProps={{
-                  shrink: true,
-                }} />
+                  shrink: true
+                }} autoComplete ="off"/>
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  label="ID"
+                  variant="outlined"
+                  fullWidth
+                  InputLabelProps={{ shrink: true }}
+                  inputProps={{
+                    pattern: "[0-9]*",
+                    onKeyPress: (event) => {
+                      if (!/[0-9]/.test(event.key)) {
+                        event.preventDefault();
+                      }
+                    },
+                  }}
+                autoComplete ="off"/>
               </Grid>
             </Grid>
             <Button variant="contained" type="submit">
