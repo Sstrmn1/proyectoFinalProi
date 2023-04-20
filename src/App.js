d844ad0e9cb6f06d51740bf24681f4b3ea2aee8d
Fabio <sstrmn1@gmail.com>
2023-04-20 10:44:07 -0400

    Refactorizar clase persona

--- src/App.js
+++ src/App.js
@@ -6,55 +6,84 @@ import Persona from "./clases/persona";
 
 
 function App() {
+  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const primerNombre = event.target.elements.primerNombre.value;
+    const segundoNombre = event.target.elements.segundoNombre.value;
+    const primerApellido = event.target.elements.primerApellido.value;
+    const segundoApellido = event.target.elements.segundoApellido.value;
+    const fechaNacimiento = event.target.elements.fechaNacimiento.value;
+    const estadoCivil = event.target.elements.estadoCivil.value;
+    const padresFamilia = event.target.elements.padresFamilia.value;
+    const hijos = event.target.elements.hijos.value;
+    const identidad = event.target.elements.identidad.value;
+
+    const nuevaPersona = new Persona(primerNombre, segundoNombre, primerApellido, segundoApellido, fechaNacimiento, estadoCivil, padresFamilia, hijos, identidad);
+    
+    console.log(nuevaPersona);
+    console.log(nuevaPersona.calcularEdad());
+    
+  };
+
+  // const handleCalcularEdad = () => {
+  //   if (nuevaPersona) {
+  //     const edad = nuevaPersona.calcularEdad();
+  //     console.log(`La edad de la persona es ${edad} años`);
+  //   }
+  // };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Formulario
         </p>
-        <form className="form">
-          <div className="form-container">
+        <form className="form" onSubmit={handleSubmit} >
+          <div className="form-container" >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField label="Primer Nombre" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="primerNombre" />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Segundo Nombre" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="segundoNombre" />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Primer Apellido" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="primerApellido" />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Segundo Apellido" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="segundoApellido" />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Fecha de Nacimiento" type="date" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="fechaNacimiento" />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Estado Civil" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="estadoCivil" />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Padres" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="padresFamilia" />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField label="Hijos" variant="outlined" fullWidth InputLabelProps={{
                   shrink: true
-                }} autoComplete ="off"/>
+                }} autoComplete="off" name="hijos" />
               </Grid>
               <Grid item xs={12} sm={12}>
                 <TextField
@@ -70,22 +99,19 @@ function App() {
                       }
                     },
                   }}
-                autoComplete ="off"/>
+                  autoComplete="off" name="identidad" />
               </Grid>
             </Grid>
             <Button variant="contained" type="submit">
               Submit
             </Button>
+
+            {/* <Button variant="contained" onClick={handleCalcularEdad}>
+              Calcular edad
+            </Button> */}
           </div>
         </form>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
--- src/clases/persona.js
+++ src/clases/persona.js
@@ -10,7 +10,90 @@ export default class Persona {
         this.hijos = hijos
         this.identidad = ident
     }
+
+
+
+    calcularEdad() {
+        const hoy = new Date();
+        const fechaNacimiento = new Date(this.fechaNacimiento);
+        let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+
+        const años = edad;
+
+        return `${años} años`;
+    }
+
+
+    /**
+ * Metodo encargado de mostrar el fin de la clase
+ */
+    static informacionClasePerson() {
+        return 'Esta es la clase para mostrar diferentes procesos de la clase persona'
+    }
+
+    /**
+     * Metodo Get encargado de retornar el nombre de la persona
+     */
+    get nombreCompleto() {
+        let nombreAll = ''
+        if (this.primerNombre !== undefined && this.primerNombre !== null && this.primerNombre.trim() !== '') {
+            nombreAll += this.primerNombre
+        }
+        if (this.segundoNombre !== undefined && this.segundoNombre !== null && this.segundoNombre.trim() !== '') {
+            nombreAll += ' ' + this.segundoNombre
+        }
+        if (this.primerApellido !== undefined && this.primerApellido !== null && this.primerApellido.trim() !== '') {
+            nombreAll += ' ' + this.primerApellido
+        }
+        if (this.segundoApellido !== undefined && this.segundoApellido !== null && this.segundoApellido.trim() !== '') {
+            nombreAll += ' ' + this.segundoApellido
+        }
+        if (nombreAll === '') {
+            nombreAll = 'No ingreso el Nombre de la persona'
+        }
+        return nombreAll
+    }
+
+    /**
+     * Metodo GET retornar los apellidos de la persona
+     */
+    get llamarApellidos() {
+        let apellidos = ''
+        if (this.primerApellido !== undefined && this.primerApellido !== null && this.primerApellido.trim() !== '') {
+            apellidos += this.primerApellido
+        }
+        if (this.segundoApellido !== undefined && this.segundoApellido !== null && this.segundoApellido.trim() !== '') {
+            apellidos += ' ' + this.segundoApellido
+        }
+        return apellidos
+    }
+
+
 }
+
+
+
+    // funcion defectuosa
+    // calcularEdad() {
+    //     const hoy = new Date();
+    //     const fechaNacimiento = new Date(this.fechaNacimiento);
+    //     let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+    //     const mesActual = hoy.getMonth();
+    //     const mesNacimiento = fechaNacimiento.getMonth();
+
+    //     if (mesActual < mesNacimiento || (mesActual == mesNacimiento && hoy.getDate() < fechaNacimiento.getDate())) {
+    //         edad--;
+    //     }
+
+    //     fechaNacimiento.setFullYear(hoy.getFullYear());
+
+    //     const diff = hoy.getTime() - fechaNacimiento.getTime();
+    //     const dias = Math.floor(diff / (1000 * 60 * 60 * 24));
+    //     const meses = Math.floor(dias / 30);
+    //     const años = edad;
+
+    //     return `${años} años, ${meses} meses y ${dias} días`;
+    // }
 
 
 // export default class Persona {
