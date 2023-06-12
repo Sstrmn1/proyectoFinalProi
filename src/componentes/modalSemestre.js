c927220f6e155eaf906d1507579c7c8fc1dfc2cf
Fabio <sstrmn1@gmail.com>
2023-06-12 14:23:45 -0400

    Todos los modales mejorados en estetica, falta un pequeño cambio en el campo profesion del modal docente

--- src/componentes/modalSemestre.js
+++ src/componentes/modalSemestre.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Semestre from "../clases/semestre";
 
-import { Button, TextField, MenuItem, Modal } from "@mui/material";
+import { Button, TextField, MenuItem, Modal, Grid } from "@mui/material";
 
 const ModalSemestre = ({ open, handleClose, addSemestre }) => {
   const [codigoSemestre, setCodigoSemestre] = useState("");
@@ -35,62 +35,96 @@ const ModalSemestre = ({ open, handleClose, addSemestre }) => {
     addSemestre(semestre);
 
     // Aquí puedes realizar cualquier acción adicional que necesites
-    
+
     handleClose(); // Cerrar el modal después de aceptar
   };
 
   return (
-    <Modal open={open} onClose={handleClose}>
+    <Modal open={open} onClose={handleClose} className="modal-form">
       <div className="modal-container">
-        <h2>Registro de Semestre</h2>
-        <div>
-          <label htmlFor="codigoSemestre">Código semestre</label>
-          <TextField
-            id="codigoSemestre"
-            variant="outlined"
-            value={codigoSemestre}
-            onChange={handleCodigoSemestreChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="numeroSemestre">Número de semestre</label>
-          <TextField
-            id="numeroSemestre"
-            variant="outlined"
-            value={numeroSemestre}
-            onChange={handleNumeroSemestreChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="cantidadMaterias">Cantidad de materias</label>
-          <TextField
-            id="cantidadMaterias"
-            variant="outlined"
-            value={cantidadMaterias}
-            onChange={handleCantidadMateriasChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="carrera">Carrera</label>
-          <TextField
-            id="carrera"
-            select
-            variant="outlined"
-            value={carrera}
-            onChange={handleCarreraChange}
-          >
-            <MenuItem value="ingenieria">Ingeniería</MenuItem>
-            <MenuItem value="ciencias">Ciencias</MenuItem>
-            <MenuItem value="arte">Arte</MenuItem>
-          </TextField>
-        </div>
-        <div className="modal-buttons">
-          <Button variant="contained" onClick={handleClose}>
-            Cancelar
-          </Button>
-          <Button variant="contained" color="primary" onClick={handleAccept}>
-            Aceptar
-          </Button>
+        <div className="modal-content">
+          <h2>Registro de Semestre</h2>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                label="Codigo de Semestre"
+                id="codigoSemestre"
+                variant="outlined"
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                autoComplete="off"
+                value={codigoSemestre}
+                onChange={handleCodigoSemestreChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Numero de semestre"
+                id="numeroSemestre"
+                variant="outlined"
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                autoComplete="off"
+                value={numeroSemestre}
+                onChange={handleNumeroSemestreChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Cantidad de materias"
+                id="cantidadMaterias"
+                variant="outlined"
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                autoComplete="off"
+                value={cantidadMaterias}
+                onChange={handleCantidadMateriasChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Carrera"
+                id="carrera"
+                select
+                variant="outlined"
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={carrera}
+                onChange={handleCarreraChange}
+              >
+                <MenuItem value="ingenieria">Ingeniería</MenuItem>
+                <MenuItem value="ciencias">Ciencias</MenuItem>
+                <MenuItem value="arte">Arte</MenuItem>
+              </TextField>
+            </Grid>
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleAccept}
+              >
+                Aceptar
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant="contained" onClick={handleClose}>
+                Cancelar
+              </Button>
+            </Grid>
+          </Grid>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div className="modal-buttons"></div>
         </div>
       </div>
     </Modal>
